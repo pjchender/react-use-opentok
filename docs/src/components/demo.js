6eeb5c4aa3d9d331a4681c93c01649ee49651ff2
andyyou <andyyu0920@gmail.com>
2020-01-13 16:34:00 +0800

    Add docs

---
+++ docs/src/components/demo.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useCallback } from 'react';
+import styled from '@emotion/styled';
+import useOpenTok from 'react-use-opentok';
+
+export default () => {
+  return (
+    <div>
+      Example
+    </div>
+  );
+};
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { Flex, Box } from '@theme-ui/components';
+import Demo from '../components/demo';
 
 export default () => (
   <Layout>
-    Hello World
+    <Flex>
+      <Box 
+        p={2}
+        sx={{
+          flex: 2,
+        }}
+      >
+        <Demo />
+      </Box>
+      <Box p={2}
+        sx={{
+          flex: 1
+        }}
+      >
+        react-use-opentok is a react hook library for Tokbox service.
+      </Box>
+    </Flex>
   </Layout>
 )
