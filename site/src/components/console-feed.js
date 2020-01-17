7f75a779082d8dbd65508fe7188a3ed0753cfbae
pjchender <pjchender@gmail.com>
2020-01-17 12:07:41 +0800

    Add ConsoleFeed component

---
+++ site/src/components/console-feed.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Hook, Console } from 'console-feed';
+import { Box, Heading } from '@theme-ui/components';
+
+const scrollToBottom = targetElement => {
+  targetElement.scrollTo({
+    top: targetElement.scrollHeight,
+    behavior: 'smooth',
+  });
+};
+
+const ConsoleFeed = () => {
+  const [logs, setLogs] = useState([]);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    Hook(
+      window.console,
+      log => {
+        setLogs(prevLogs => [...prevLogs, log]);
+        scrollToBottom(containerRef.current);
+      },
+      false
+    );
+  }, []);
+
+  return (
+    <>
+      <Heading as="h3" mt="4">
+        Browser Console
+      </Heading>
+      <Box
+        ref={containerRef}
+        my="3"
+        sx={{
+          backgroundColor: '#242424',
+          maxHeight: 200,
+          overflow: 'auto',
+        }}
+      >
+        <Console logs={logs} variant="dark" />
+      </Box>
+    </>
+  );
+};
+
+export default ConsoleFeed;
